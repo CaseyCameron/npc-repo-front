a3acec025608b0100de394f11a634b986cd698e0
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-06 11:17:18 -0700

    App.js functional component refactor

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Redirect, Route, Link } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 import Home from '../home/Home';
@@ -7,36 +8,24 @@ import NpcDetailPage from '../npc/NpcDetailPage';
 import NpcEditPage from '../npc-edit/NpcEditPage';
 import NpcsPage from '../npcs/NpcsPage';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
-
 import './App.css';
 
-class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/npcs" component={NpcsPage} />
-            <Route exact path="/npcs/add" component={NpcAddPage} />
-            <Route exact path="/npcs/:id" component={NpcDetailPage} />
-            <Route path="/npcs/:id/edit" component={NpcEditPage} />
-            <Redirect to="/" />
-          </Switch>
-          <Footer />
-        </Router>
-      </div>
-    );
-  }
-
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/npcs" component={NpcsPage} />
+          <Route exact path="/npcs/add" component={NpcAddPage} />
+          <Route exact path="/npcs/:id" component={NpcDetailPage} />
+          <Route path="/npcs/:id/edit" component={NpcEditPage} />
+          <Redirect to="/" />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App;
